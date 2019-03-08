3fbf4a1fb6b7d5762f5c80e80bb53837942f5264
Rupert brax Schuppe <rschuppe@e1r11p4.21-school.ru>
2019-03-08 15:41:22 +0300

    update draw_circle

--- srcs/ft_draw_circle.c
+++ srcs/ft_draw_circle.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_draw_circle.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wballaba <wballaba@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rschuppe <rschuppe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/22 18:33:17 by rschuppe          #+#    #+#             */
-/*   Updated: 2019/02/26 19:27:23 by wballaba         ###   ########.fr       */
+/*   Updated: 2019/03/08 15:41:03 by rschuppe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,29 @@ void	ft_draw_circle(t_mlx *visualiser, t_params *data, int color)
 {
 	double	val;
 	double	angle;
+	double	offset;
 	int		pos[2];
 
 	val = PI / 180;
-	angle = 0;
-	while (angle < 360)
+	offset = -(data->line_width / 2);
+	while (offset < data->line_width / 2.0)
 	{
-		pos[0] = data->x + round(data->radius * cos(angle * val));
-		pos[1] = data->y + round(data->radius * sin(angle * val));
-		if (data->img)
-			data->img->data[pos[1] * (data->img->size_l / 4) + pos[0]] = color;
-		else
-			mlx_pixel_put(visualiser->mlx_ptr, visualiser->win_ptr,
-				pos[0], pos[1], color);
-		angle++;
+		angle = 0;
+		while (angle < 360)
+		{
+			pos[0] =
+				data->x + round((data->radius + offset) * cos(angle * val));
+			pos[1] =
+				data->y + round((data->radius + offset) * sin(angle * val));
+			if (data->img)
+				data->img->data[pos[1] * (data->img->size_l / 4) + pos[0]] =
+					color;
+			else
+				mlx_pixel_put(visualiser->mlx_ptr, visualiser->win_ptr,
+					pos[0], pos[1], color);
+			angle++;
+		}
+		offset++;
 	}
 	return ;
 }
