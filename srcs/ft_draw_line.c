8bef53cef13143799e50a2c63c75e8a24d8992fe
Watty Ballabar <wballaba@e1r11p2.21-school.ru>
2019-03-09 16:19:37 +0300

    fix draw line

--- srcs/ft_draw_line.c
+++ srcs/ft_draw_line.c
@@ -6,7 +6,7 @@
 /*   By: wballaba <wballaba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/23 14:21:21 by wballaba          #+#    #+#             */
-/*   Updated: 2019/03/09 16:10:25 by wballaba         ###   ########.fr       */
+/*   Updated: 2019/03/09 16:19:16 by wballaba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ static void	ft_draw_line_one_pix(t_mlx *visualiser, t_params *data, int color,
 		pos[1] = round(data->y + length * sin_a);
 		if (pos[0] >= 0 && pos[0] < win_size &&
 			pos[1] >= 0 && pos[1] < win_size)
-			draw_one_pix_circle(visualiser, data, color, pos);
+			draw_one_pix_line(visualiser, data, color, pos);
 		length--;
 	}
 }
